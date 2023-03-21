df123ae45fd470ae90ac684b66ccaba3a8d14699
Magda <magdaelsayed03@gmail.com>
2023-03-21 16:13:43 -0700

    alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,10 +8,8 @@
 
 void print_alphabet(void)
 {
-	char letters;
-	for (letters = 'a'; letters <= 'z'; letters++);
-
-		_putchar(letters);
-	
+	int ch;
+	for (ch = 'a'; ch <= 'z'; ch++);
+		_putchar(ch);
 	_putchar('\n');
 }
