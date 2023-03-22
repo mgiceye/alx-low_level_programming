b2a0897138ae910157d0252deb39a74f5d1b65be
Magda <magdaelsayed03@gmail.com>
2023-03-22 15:33:21 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,7 +10,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(+);
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
@@ -18,9 +18,9 @@ int print_sign(int n)
 		_putchar(0);
 		return (0);
 	}
-	else (n < 0)
+	else if (n < 0)
 	{
-		_putchar(-1);
+		_putchar('-');
 		return (-1);
 	}
 }
