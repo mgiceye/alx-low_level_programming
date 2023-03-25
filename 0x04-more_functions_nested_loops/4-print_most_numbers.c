85e90a363457557b5b166feb8f05fa84e771b033
Magda <magdaelsayed03@gmail.com>
2023-03-24 17:35:52 -0700

    believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main"
+/**
+ *print_most_numbers - function that prints the
+ *numbers, from 0 to 9,
+ *followed by a new line.
+ *
+ *Return: 0.
+ */
+
+void print_most_numbers(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+
+		if (num != '2' && num != '4')
+		{
+
+		_putchar(num);
+		}
+	_putchar('\n');
+}
