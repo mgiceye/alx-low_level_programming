c70b9cefbde7f48c33982d32e29a4cef13682be5
Magda <magdaelsayed03@gmail.com>
2023-03-28 05:13:10 -0700

    3 & 4

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -10,6 +10,8 @@
 void _puts(char *str)
 {
 	while (*str != '\0')
+	{
 		_putchar(*str++);
+	}
 		_putchar('\n');
 }
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main"
+/**
+ *print_rev - reverses a string.
+ *@s: string
+ *Return:0.
+ */
+
+void print_rev(char *s)
+{
+	int len;
+	int i;
+
+	len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	s--;
+
+	for (i = len; i >= 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
