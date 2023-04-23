a765f34c0a77434517a6f2ee44226512baadadfa
Magda <magdaelsayed03@gmail.com>
2023-04-23 15:54:54 -0700

    6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,5 +12,10 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (actual_prime(n, n - 1));
+	for (int i = 2; i <= n / 2; i++)
+	{
+		if (n % i == 0)
+			return (0);
+	}
+	return (1);
 }
