8547ab05a580393afc1737479662dff6a71e2cff
Mg <magdaelsayed03@gmail.com>
2023-06-09 13:36:12 -0700

    first things first

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+*main - a program that prints its name.
+*@argv: argument passed.
+*@argc: argument counter.
+* Return: 0.
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	printf('\n');
+	return (0);
+}
