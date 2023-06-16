dc21456032baeae212940dc437254697965309d5
Mg <magdaelsayed03@gmail.com>
2023-06-15 18:13:33 -0700

    initializing

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *malloc_checked - a function that allocates
+ *memory using malloc.
+ *
+ *@b: unsigned int
+ * Return: pointer.
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
