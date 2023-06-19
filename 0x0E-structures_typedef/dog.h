11f9f9f27213071cac2e871c63a79a366242a09d
Mag <magdaelsayed03@gmail.com>
2023-06-20 00:12:08 +0300

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - define struct
+ * @name: First member
+ * @age: Second member
+ *@owner: third member
+ * Description: a dog's information
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
